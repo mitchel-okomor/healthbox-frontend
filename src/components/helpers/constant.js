350db0106ca7625383d192d536c7576cfe5cc62b
CodeXaver <mitchel.okomor@gmail.com>
2021-04-25 10:29:39 +0100

    host url

--- src/components/helpers/constant.js
+++ src/components/helpers/constant.js
@@ -1,6 +1,7 @@
-export const SERVER_URL = "http://localhost:5000";
-//https://instantinx.herokuapp.com
+//export const SERVER_URL = "http://localhost:5000";
+export const SERVER_URL = "https://jomedhealthbox.herokuapp.com/";
 export const SET_LOADING = "SET_LOADING";
+
 export const SET_USER = "SET_USER";
 export const SET_CART = "SET_CART";
 export const SET_PRESCRIPTIONS = "SET_PRESCRIPTIONS";
